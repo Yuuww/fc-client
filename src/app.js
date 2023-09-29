82dc8284e23ddb07b1097ad3126a70cf69f6462b
Yuna <paul.fabian.bruhn@gmail.com>
2023-09-29 19:20:01 +0200

    Sign Up stuff

--- src/app.js
+++ src/app.js
@@ -101,7 +101,60 @@ function logIn() {
 }
 
 // Sign in
-function signUp() {}
+const signUpInputName = document.getElementById("signup-name");
+const signUpInputOne = document.getElementById("signup-password-one");
+const signUpShowPathOne = document.getElementById("signup-show-path-one");
+const signUpHidePathOne = document.getElementById("signup-hide-path-one");
+const signUpInputTwo = document.getElementById("signup-password-two");
+const signUpShowPathTwo = document.getElementById("signup-show-path-two");
+const signUpHidePathTwo = document.getElementById("signup-hide-path-two");
+const signUpError = document.getElementById("signup-error");
+function toggleSignUpPasswordVisabilityOne() {
+  signUpShowPathOne.classList.toggle("hidden");
+  signUpHidePathOne.classList.toggle("hidden");
+  if (signUpInputOne.type === "password") {
+    signUpInputOne.type = "text";
+  } else {
+    signUpInputOne.type = "password";
+  }
+}
+function toggleSignUpPasswordVisabilityTwo() {
+  signUpShowPathTwo.classList.toggle("hidden");
+  signUpHidePathTwo.classList.toggle("hidden");
+  if (signUpInputTwo.type === "password") {
+    signUpInputTwo.type = "text";
+  } else {
+    signUpInputTwo.type = "password";
+  }
+}
+function signUp() {
+  let signUpName = signUpInputName.value;
+  let signUpPasswordOne = signUpInputOne.value;
+  let signUpPasswordTwo = signUpInputTwo.value;
+  if (signUpPasswordOne !== signUpPasswordTwo) {
+    signUpError.innerText = "Error: Passwords do not match.";
+    return;
+  }
+  fetch("https://api.get-done.de:3001/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ access: flashcardData.access, account: logInName, password: signUpPasswordOne }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.response) {
+        flashcardData.accounts[signUpName] = data.account;
+        console.log(flashcardData);
+        switchView(stacksView);
+      } else {
+        signUpError.innerText = `Error: ${data.error}`;
+      }
+    })
+    .catch((error) => {
+      console.error("Fehler bei der API-Anfrage:", error);
+      signUpError.innerText = "Error: API dose not response.";
+    });
+}
 
 function switchAccount() {}
 // Stacks
